063a45dc4393bd090db92ebe55c6423bfeecfb52
SORmaker <1583211717@qq.com>
2023-04-20 16:23:02 +0800

    hot fix

---
+++ 2.Firmware/1.CH32V307VCT6/project/mrs/obj/sdk/Startup/subdir.mk
@@ -0,0 +1,21 @@
+################################################################################
+# MRS Version: {"version":"1.8.4","date":"2023/02/015"}
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_UPPER_SRCS += \
+S:/WorkSpace/bicycle\ motocross/CH32-Bike-Overland/2.Firmware/1.CH32V307VCT6/libraries/sdk/Startup/startup_ch32v30x_D8C.S 
+
+OBJS += \
+./sdk/Startup/startup_ch32v30x_D8C.o 
+
+S_UPPER_DEPS += \
+./sdk/Startup/startup_ch32v30x_D8C.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+sdk/Startup/startup_ch32v30x_D8C.o: S:/WorkSpace/bicycle\ motocross/CH32-Bike-Overland/2.Firmware/1.CH32V307VCT6/libraries/sdk/Startup/startup_ch32v30x_D8C.S
+	@	@	riscv-none-embed-gcc -march=rv32imafc -mabi=ilp32f -msmall-data-limit=8 -mno-save-restore -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -pedantic -Wunused -Wuninitialized -Wall  -g -x assembler -I"S:\WorkSpace\bicycle motocross\CH32-Bike-Overland\2.Firmware\1.CH32V307VCT6\libraries\sdk\Startup" -I"S:\WorkSpace\bicycle motocross\CH32-Bike-Overland\2.Firmware\1.CH32V307VCT6\libraries\zf_driver" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+
